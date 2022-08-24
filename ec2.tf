3e82e9d4c35804deaa75eedfaa5c11e11f3d677a
Pavan544 <47830295+Pavan544@users.noreply.github.com>
2022-08-24 14:49:54 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,65 @@
-provider "aws"{
+#provider
+provider "aws" {
   region="ap-south-1"
 }
-resource "aws_instance" "foo" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
-  instance_type = "t2.micro"
+resource "aws_vpc" "vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
 }
+
+resource "aws_subnet" "Private" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Private"
+  }
+}
+
+        resource "aws_subnet" "Public" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Public"
+  }
+}
+resource "aws_security_group" "tsg" {
+  name        = "terraform-sg"
+  description = "Default SG to alllow traffic from the VPC"
+  vpc_id      = aws_vpc.vpc.id
+  depends_on = [
+    aws_vpc.vpc
+  ]
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.vpc.cidr_block]
+  }
+
+  egress {
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+    cidr_blocks = [aws_vpc.vpc.cidr_block]
+  }
+
+  tags = {
+    Name = "allow-ssh"
+  }
+}
+
+resource "aws_instance" "web1" {
+    ami = "ami-06489866022e12a14"
+    instance_type = "t2.micro"
+    subnet_id              = aws_subnet.Public.id
+    vpc_security_group_ids = [aws_security_group.tsg.id]
+
+    }
