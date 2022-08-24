be25f91ec2f5f2ed08571157c15565f4f24531ac
Pavan544 <47830295+Pavan544@users.noreply.github.com>
2022-08-24 17:39:18 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -62,24 +62,4 @@ resource "aws_instance" "web1" {
     subnet_id              = aws_subnet.Public.id
     vpc_security_group_ids = [aws_security_group.tsg.id]
     key_name = "tf-key"
-
     }
-provisioner "remote-exec" {
-    inline = [
-      "touch hello.txt",
-      "echo helloworld remote provisioner >> hello.txt",
-    ]
-  }
-  connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = "ec2-user"
-      private_key = file("/home/ec2-user/.ssh/id_rsa")
-      timeout     = "4m"
-   }
-}
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "tf-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDAPaE2c14h0ReFhPAllFTgZxd1w/5o+5ceMe59ecU8Ee4PRg9Zk1lYFpwNp9m7aCTfFKC1PwCEzulHL+IRNQDTxtw87GTU+B1JqoVfhEg9HGd1KQP+iPw5YPD8QXf+HWE8CPY8RvPdObcFxE6uZqdg62nbMVex529UNpPZfXq1FI+uD03lGLMCYyZRGRqC5oDsQutPfJBfYjn2WB/gkzODnf1oyBgYYl4InF4Vd3G8DZFqObHcNBLjTKjJ/3M1JOXzZncLoditBubFHJUXPh1o6tghlYDEzkvBL6AuKfCbi+imOVDsB9Hw5Z4AnzGZwjEAa2xAim5FOwgv/mOISk9Z ec2-user@ip-172-31-6-198.ap-south-1.compute.internal"
-}
